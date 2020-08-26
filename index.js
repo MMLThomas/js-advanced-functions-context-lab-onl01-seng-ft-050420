5c0ed6300f8e02e64727f9660816e62e755c7c14
michael thomas <michael.thomas2014@gmail.com>
2020-08-26 00:50:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,52 @@ function createTimeInEvent(ds){
     return this
 }
 
+function createTimeOutEvent(obj, ds){
+  let dateTime = ds.split(' ')
+  this.timeOutEvents.push(
+    {
+      type: 'TimeOut',
+      hour: parseInt(dateTime[1]),
+      date: dateTime[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate(ds){
+  function time(element){
+    return (element.date === ds);
+  }
+  
+  
+  let timeIn = this.timeInEvents.find(time)
+  let timeOut = this.timeOutEvents.find(time)
+  
+  return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(ds){
+  let hoursWorked = hoursWorkedOnDate(ds)
+  let pay = this.payPerHour
+  return (hoursWorked * pay)
+}
+
+function allWagesFor(obj){
+  let days = obj.timeInEvents.map(element => element.date)
+  let wages = days.map(element => wagesEarnedOnDate(obj, element))
+  return wages.reduce(function(total, element){return element + total},0);
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+  function match(element){
+    return (element.firstName === firstName);
+  }
+  return srcArray.find(match)
+}
+
+function calculatePayroll(arr){
+  return arr.reduce(function(total,element){return allWagesFor(element) + total}, 0);
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
